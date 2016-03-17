be5553c7b3ec56c367b4cd41415448ff93d08cc7
jhwebtech <julieh71@ymail.com>
2016-03-17 14:26:05 -0500

    Updated AWS access

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,10 +8,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.prodution?
+  if Rails.env.production?
     storage :fog
-  else  
-    storage :file
+  else
+    storage :file 
   end
   # storage :fog
 
